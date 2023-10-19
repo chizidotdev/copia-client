58e844b45232130733206a95fa4dc42a8b160c4d
Prince Muel <vansomecsam@gmail.com>
2023-10-19 04:37:50 +0100

    deps: add rq packages
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,11 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ['@remix-run/eslint-config', '@remix-run/eslint-config/node'],
+  extends: [
+    '@remix-run/eslint-config',
+    '@remix-run/eslint-config/node',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+  ],
+  plugins: ['@tanstack/query'],
   rules: {
     '@typescript-eslint/no-unused-vars': 'warn',
   },
