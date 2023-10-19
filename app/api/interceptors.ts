8854abdc6e7dd10d0f8050af8b898fc71cdf65e5
Prince Muel <vansomecsam@gmail.com>
2023-10-19 02:42:47 +0100

    style: format files with prettier
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/api/interceptors.ts
+++ app/api/interceptors.ts
@@ -3,27 +3,24 @@ import { useNavigate } from '@remix-run/react';
 import { useEffect } from 'react';
 
 export function useInterceptors() {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
   useEffect(() => {
-    axios.interceptors.request.use(
-      async (config) => {
-        config.headers['Content-Type'] = 'application/json';
-        config.withCredentials = true;
-        return config;
-      },
-    );
+    axios.interceptors.request.use(async (config) => {
+      config.headers['Content-Type'] = 'application/json';
+      config.withCredentials = true;
+      return config;
+    });
 
     // navigate to login page if 401 error
     axios.interceptors.response.use(
-        (response) => response,
-        (error) => {
-            if (error.response.status === 401) {
-                navigate('/u/login');
-            }
-            return Promise.reject(error);
+      (response) => response,
+      (error) => {
+        if (error.response.status === 401) {
+          navigate('/u/login');
         }
+        return Promise.reject(error);
+      }
     );
-
   }, []);
 }
--- app/api/inventory.ts
+++ app/api/inventory.ts
@@ -3,6 +3,6 @@ import { Inventory } from '~lib/types';
 import axios from 'axios';
 
 export const getInventory = async (): Promise<Inventory> => {
-    const response = await axios.get(`${BASE_URL}/inventory`);
-    return response.data;
+  const response = await axios.get(`${BASE_URL}/inventory`);
+  return response.data;
 };
