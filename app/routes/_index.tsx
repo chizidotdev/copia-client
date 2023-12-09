250407edbba75f7669040d18e0416f5774028905
Chizi Victor <chiziwokoma@gmail.com>
2023-12-09 13:16:43 +0100

    feat: verification email sent alert

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,19 +3,16 @@ import { Link } from '@remix-run/react';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: 'New Remix App' },
-    { name: 'description', content: 'Welcome to Remix!' },
+    { title: 'Copia' },
+    { name: 'description', content: 'Welcome to Copia!' },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1>Welcome to Remix</h1>
+    <div style={{ lineHeight: '1.8' }}>
+      <h1>Welcome to Copia</h1>
       <ul>
-        <li>
-          <Link to='/u/login'>Login</Link>
-        </li>
         <li>
           <Link to='/dashboard'>Dashboard</Link>
         </li>
