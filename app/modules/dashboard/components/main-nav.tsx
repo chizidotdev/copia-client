df7ed139a84d8755117dcfef688c5158099aa857
Chizi Victor <chiziwokoma@gmail.com>
2023-12-15 22:57:38 +0100

    feat: add active state to main navbar

--- app/modules/dashboard/components/main-nav.tsx
+++ app/modules/dashboard/components/main-nav.tsx
@@ -1,35 +1,33 @@
 import { AppLogo } from '@/components/app-logo';
 import { cn } from '@/lib/utils';
-import { Link } from '@remix-run/react';
+import { NavLink } from '@remix-run/react';
 
 export function MainNav({ className }: { className?: string }) {
   return (
     <nav className={cn('flex items-center space-x-4 lg:space-x-6', className)}>
       <AppLogo />
-      <Link
-        to='/dashboard'
-        className='text-sm font-medium transition-colors hover:text-primary'
-      >
-        Overview
-      </Link>
-      <Link
-        to='/dashboard'
-        className='hidden text-sm font-medium text-muted-foreground transition-colors hover:text-primary sm:block'
-      >
-        Customers
-      </Link>
-      <Link
-        to='/products'
-        className='hidden text-sm font-medium text-muted-foreground transition-colors hover:text-primary sm:block'
-      >
-        Products
-      </Link>
-      <Link
-        to='/dashboard'
-        className='hidden text-sm font-medium text-muted-foreground transition-colors hover:text-primary sm:block'
-      >
-        Settings
-      </Link>
+
+      {links.map(({ href, label }) => (
+        <NavLink
+          key={`${href}${label}`}
+          to={href}
+          className={({ isActive }) =>
+            cn(
+              'text-sm font-medium transition-colors hover:text-primary',
+              !isActive && 'text-muted-foreground'
+            )
+          }
+        >
+          {label}
+        </NavLink>
+      ))}
     </nav>
   );
 }
+
+const links = [
+  { href: '/dashboard', label: 'Overview' },
+  { href: '/products', label: 'Products' },
+  // { href: '/dashboard', label: 'Customers' },
+  // { href: '/dashboard', label: 'Settings' },
+];
